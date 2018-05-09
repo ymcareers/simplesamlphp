3aa01fbb0ca6ea061134925ede9e3e1198a7548c
Shruti Nafday <SNafday@ymadmins-mbp-7.corp.yourmembership.com>
2018-05-09 16:59:18 -0400

    added config

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = array(
+        'logging.handler' => 'errorlog',
+        'logging.level'  => \SimpleSAML\Logger::DEBUG,
+        'errorreporting' => false,
+	'debug'          => false,
+);
